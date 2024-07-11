569a885b22a4ed0ac9dfd6ab1e7dcd55c2885856
Ayrton Munoz <a.munoz3327@gmail.com>
2024-07-11 14:21:04 -0400

    WIP dockchannel hid stuff
    We need to upload the MTP firmware so I'm not sure I can get the keyboard will
    working before being able to mount root. Another option is to embed the fw in
    the kernel image (or u-boot?), but I need to double check the size to see if
    that's feasible.

--- sys/arm64/apple/apple_dockchannel.c
+++ sys/arm64/apple/apple_dockchannel.c
@@ -1,3 +1,29 @@
+/*-
+ * Copyright (c) 2024 Ayrton Muñoz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include "opt_platform.h"
 
 #include <sys/cdefs.h>
@@ -50,11 +76,6 @@ struct apple_dockchannel_softc {
 	void *sc_intrhand[MAX_INTR];
 };
 
-static struct ofw_compat_data compat_data[] = {
-	{ "apple,dockchannel",	1 },
-	{ NULL,		0 },
-};
-
 static struct resource_spec apple_dockchannel_res_spec[] = {
 	{ SYS_RES_MEMORY, 0, RF_ACTIVE },
 	{ SYS_RES_IRQ, 0, RF_ACTIVE },
@@ -63,6 +84,7 @@ static struct resource_spec apple_dockchannel_res_spec[] = {
 
 static device_probe_t apple_dockchannel_probe;
 static device_attach_t apple_dockchannel_attach;
+#if 0
 static device_detach_t apple_dockchannel_detach;
 
 static pic_disable_intr_t apple_dockchannel_disable_intr;
@@ -71,6 +93,7 @@ static pic_map_intr_t apple_dockchannel_map_intr;
 static pic_setup_intr_t apple_dockchannel_setup_intr;
 static pic_teardown_intr_t apple_dockchannel_teardown_intr;
 
+#endif
 static void apple_dockchannel_intr(void *);
 
 static int
@@ -79,7 +102,7 @@ apple_dockchannel_probe(device_t dev)
 	if (!ofw_bus_status_okay(dev))
 		return (ENXIO);
 
-	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
+	if (!ofw_bus_is_compatible(dev, "apple,dockchannel"))
 		return (ENXIO);
 
 	device_set_desc(dev, "Apple Dockchannel");
@@ -89,14 +112,14 @@ apple_dockchannel_probe(device_t dev)
 static int
 apple_dockchannel_attach(device_t dev)
 {
-	phandle_t node;
+	//phandle_t node;
 	struct apple_dockchannel_softc *sc;
-	int error;
+	//int error;
 
 	sc = device_get_softc(dev);
 	sc->sc_dev = dev;
 
-	node = ofw_bus_get_node(dev);
+	//node = ofw_bus_get_node(dev);
 
 	if (bus_alloc_resources(dev, apple_dockchannel_res_spec, sc->sc_res) != 0) {
 		device_printf(dev, "cannot allocate device resources\n");
@@ -107,7 +130,7 @@ apple_dockchannel_attach(device_t dev)
 	HWRITE4(sc, IRQ_MASK, 0);
 	HWRITE4(sc, IRQ_STAT, 0xffffffff);
 
-	if (bus_setup_intr(dev, sc->sc_res[APPLE_PINCTRL_IRQRES],
+	if (bus_setup_intr(dev, sc->sc_res[APPLE_DOCKCHANNEL_IRQRES],
 		INTR_TYPE_CLK | INTR_MPSAFE, NULL, apple_dockchannel_intr, sc,
 		sc->sc_intrhand)) {
 		device_printf(dev, "can't establish interrupt\n");
@@ -144,6 +167,36 @@ apple_dockchannel_intr(void *arg)
 	HWRITE4(sc, IRQ_STAT, stat);
 	return;
 }
+#if 0
+
+static void
+apple_dockchannel_enable_intr(device_t dev, struct intr_irqsrc *isrc)
+{
+}
+
+static void
+apple_dockchannel_disable_intr(device_t dev, struct intr_irqsrc *isrc)
+{
+}
+
+static int
+apple_dockchannel_map_intr(device_t dev, struct intr_map_data *data,
+    struct intr_irqsrc **isrcp)
+{
+}
+
+static int
+apple_dockchannel_setup_intr(device_t dev, struct intr_irqsrc *isrc,
+    struct resource *res, struct intr_map_data *data)
+{
+}
+
+static int
+apple_dockchannel_teardown_intr(device_t dev, struct intr_irqsrc *isrc,
+    struct resource *res, struct intr_map_data *data)
+{
+}
+#endif
 
 static device_method_t apple_dockchannel_methods[] = {
 	DEVMETHOD(device_probe, apple_dockchannel_probe),
@@ -168,3 +221,143 @@ static driver_t apple_dockchannel_driver = {
 };
 
 DRIVER_MODULE(apple_dockchannel, simplebus, apple_dockchannel_driver, 0, 0);
+
+#define APPLE_DOCKCHANNEL_HID_NRES 4
+//#define APPLE_DOCKCHANNEL_HID_IRQRES 4
+#define APPLE_DOCKCHANNEL_HID_CONFIG 0
+#define APPLE_DOCKCHANNEL_HID_DATA 1
+#define APPLE_DOCKCHANNEL_HID_RMT_CONFIG 2
+#define APPLE_DOCKCHANNEL_HID_RMT_DATA 3
+
+struct apple_dockchannel_hid_softc {
+	device_t sc_dev;
+	struct resource *sc_mem_res[APPLE_DOCKCHANNEL_HID_NRES];
+	struct resource *sc_irq_res;
+
+	void *sc_intrhand;
+}
+
+static struct resource_spec apple_dockchannel_hid_res_spec[] = {
+	{ SYS_RES_MEMORY, APPLE_DOCKCHANNEL_HID_CONFIG, RF_ACTIVE },
+	{ SYS_RES_MEMORY, APPLE_DOCKCHANNEL_HID_DATA, RF_ACTIVE },
+	{ SYS_RES_MEMORY, APPLE_DOCKCHANNEL_HID_RMT_CONFIG, RF_ACTIVE },
+	{ SYS_RES_MEMORY, APPLE_DOCKCHANNEL_HID_RMT_DATA, RF_ACTIVE },
+//	{ SYS_RES_IRQ, 0, RF_ACTIVE },
+	{ -1, 0, 0 },
+};
+
+static device_probe_t apple_dockchannel_hid_probe;
+static device_attach_t apple_dockchannel_hid_attach;
+#if 0
+static device_detach_t apple_dockchannel_hid_detach;
+#endif
+static void apple_dockchannel_hid_intr(void *);
+
+static int
+apple_dockchannel_hid_probe(device_t dev)
+{
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
+	if (!ofw_bus_is_compatible(dev, "apple,dockchannel-hid"))
+		return (ENXIO);
+
+	device_set_desc(dev, "Apple Dockchannel HID");
+	return (BUS_PROBE_DEFAULT);
+}
+
+static int
+apple_dockchannel_hid_attach(device_t dev)
+{
+	phandle_t node;
+	int rid;
+	struct apple_dockchannel_hid_softc *sc;
+
+	sc = device_get_softc(dev);
+	sc->sc_dev = dev;
+
+	node = ofw_bus_get_node(dev);
+
+	if (bus_alloc_resources(dev, apple_dockchannel_hid_res_spec, sc->sc_mem_res) != 0) {
+		device_printf(dev, "cannot allocate device memory resources\n");
+		return (ENXIO);
+	}
+
+	if (ofw_bus_find_string_index(node, "interrupt-names", "rx", &rid) != 0) {
+		device_printf(dev, "could not find rx irq\n");
+		goto error;
+	}
+
+	sc->sc_irq_res = bus_alloc_resources_any(dev, SYS_RES_IRQ, &rid, RF_ACTIVE);
+	if (sc->sc_irq_res == NULL) {
+		device_printf(dev, "cannot allocate device irq resource\n");
+		goto error
+	}
+
+	bus_setup_intr(dev, sc->sc_irq_res, INTR_MPSAFE | INTR_TYPE_MISC /* INTR_TYPE_TTY? */,
+		apple_dockchannel_hid_intr, NULL, sc, &sc->sc_intrhand);
+
+#if 0
+	phandle = OF_getpropint(faa->fa_node, "apple,helper-cpu", 0);
+	if (phandle) {
+		error = aplrtk_start(phandle);
+		if (error) {
+			printf(": can't start helper CPU\n");
+			return;
+		}
+	}
+#endif
+
+	return (0);
+error:
+	bus_release_resources(dev, apple_dockchannel_hid_res_spec, sc->sc_res);
+	return (ENXIO);
+}
+
+struct mtp_hdr {
+	uint8_t hdr_len;
+	uint8_t chan;
+	uint16_t pkt_len;
+	uint8_t seq;
+	uint8_t iface;
+	uint16_t pad;
+} __packed;
+
+static void
+apple_dockchannel_hid_read(struct apple_dockchannel_hid_softc *sc, void *buf, size_t len,
+	uint32_t *cksum)
+{
+}
+
+// this is the rx interrupt handler... do we need a tx handler at some point?
+static void
+apple_dockchannel_hid_intr(void *arg)
+{
+	struct apple_dockchannel_hid_softc *sc = arg;
+	struct mtp_hdr hdr;
+	uint32_t cksum = 0;
+	char buf[1024];
+
+	apple_dockchannel_hid_read(sc, &hdr, sizeof(hdr), &cksum);
+	apple_dockchannel_hid_read(sc, buf, hdr.pkt_len + 4, &cksum);
+	if (cksum != 0xffffffff) {
+		return;
+	}
+}
+
+static device_method_t apple_dockchannel_hid_methods[] = {
+	DEVMETHOD(device_probe, apple_dockchannel_hid_probe),
+	DEVMETHOD(device_attach, apple_dockchannel_hid_attach),
+#if 0
+	DEVMETHOD(device_detach, apple_dockchannel_detach),
+#endif
+
+	DEVMETHOD_END
+};
+
+static driver_t apple_dockchannel_hid_driver = {
+	.name = "apple dockchannel hid",
+	.methods = apple_dockchannel_hid_methods,
+	.size = sizeof(struct apple_dockchannel_hid_softc),
+};
+DRIVER_MODULE(apple_dockchannel_hid, simplebus, apple_dockchannel_hid_driver, 0, 0);
