e1435035ca8aed3c0308975bb9f63daac0de55df
Ayrton Munoz <a.munoz3327@gmail.com>
2024-08-06 08:02:10 -0400

    dockchannel pic stuff
    Probably need locks around access to dc mmio and to cleanup the error
    paths, but after that I think I should be good to move on to the
    dockchannel hid part.

--- sys/arm64/apple/apple_dc.c
+++ sys/arm64/apple/apple_dc.c
@@ -57,10 +57,15 @@
 
 #define MAX_INTR	32
 
-#define HREAD4(sc, reg)	\
-	bus_read_4((sc)->res[APPLE_DOCKCHANNEL_MEMRES], reg)
-#define HWRITE4(sc, reg, val)	\
-	bus_write_4((sc)->res[APPLE_DOCKCHANNEL_MEMRES], reg, val)
+#define HREAD4(sc, reg) \
+	bus_read_4((sc)->sc_res[APPLE_DOCKCHANNEL_MEMRES], reg)
+#define HWRITE4(sc, reg, val) \
+	bus_write_4((sc)->sc_res[APPLE_DOCKCHANNEL_MEMRES], reg, val)
+
+#define HSET4(sc, reg, bit) HWRITE4(sc, reg, HREAD4(sc, reg) | (1 << bit))
+
+#define HCLEAR4(sc, reg, bit) \
+	HWRITE4(sc, reg, HREAD4(sc, reg) & ~(1 << bit))
 
 enum {
 	APPLE_DOCKCHANNEL_MEMRES = 0,
@@ -69,17 +74,18 @@ enum {
 };
 
 struct apple_dc_irqsrc {
-	struct intr_irqsrc	isrc; /* base class must be first */
-	int					irq;
+	struct intr_irqsrc	di_isrc; /* base class must be first */
+	uint32_t			di_irq;
+	uint32_t			di_level;
 };
 
 struct apple_dc_softc {
-	device_t		dev;
+	device_t		sc_dev;
 
-	struct resource	*res[APPLE_DOCKCHANNEL_NRES];
-	void			*intrhand;
+	struct resource	*sc_res[APPLE_DOCKCHANNEL_NRES];
+	void			*sc_intrhand;
 
-	struct apple_dc_irqsrc	dcisrcs[MAX_INTR];
+	struct apple_dc_irqsrc	sc_isrcs[MAX_INTR];
 };
 
 static struct resource_spec apple_dc_res_spec[] = {
@@ -98,7 +104,21 @@ static pic_map_intr_t apple_dc_map_intr;
 static pic_setup_intr_t apple_dc_setup_intr;
 static pic_teardown_intr_t apple_dc_teardown_intr;
 
-static void apple_dc_intr(void *);
+static void
+apple_dc_mask_irq(struct apple_dc_softc *sc, uint32_t irq)
+{
+	MPASS(irq < MAX_INTR);
+	HCLEAR4(sc, IRQ_MASK, irq);
+}
+
+static void
+apple_dc_unmask_irq(struct apple_dc_softc *sc, uint32_t irq)
+{
+	MPASS(irq < MAX_INTR);
+	HSET4(sc, IRQ_MASK, irq);
+}
+
+static int apple_dc_intr(void *);
 
 static int
 apple_dc_probe(device_t dev)
@@ -116,29 +136,55 @@ apple_dc_probe(device_t dev)
 static int
 apple_dc_attach(device_t dev)
 {
+	int irq, rc;
 	struct apple_dc_softc *sc = device_get_softc(dev);
-	sc->dev = dev;
+	const char *name;
+	phandle_t node;
 
-	if (bus_alloc_resources(dev, apple_dc_res_spec, sc->res) != 0) {
+	sc->sc_dev = dev;
+	node = ofw_bus_get_node(sc->sc_dev);
+
+	if (bus_alloc_resources(dev, apple_dc_res_spec, sc->sc_res) != 0) {
 		device_printf(dev, "cannot allocate device resources\n");
 		return (ENXIO);
 	}
 
-	/* disable and clear interrupts */
+	/* mask and clear interrupts */
 	HWRITE4(sc, IRQ_MASK, 0);
 	HWRITE4(sc, IRQ_STAT, 0xffffffff);
 
-	if (bus_setup_intr(dev, sc->res[APPLE_DOCKCHANNEL_IRQRES],
-		INTR_TYPE_CLK | INTR_MPSAFE, NULL, apple_dc_intr, sc,
-		&sc->intrhand)) {
+	if (bus_setup_intr(dev, sc->sc_res[APPLE_DOCKCHANNEL_IRQRES],
+		INTR_TYPE_CLK | INTR_MPSAFE, apple_dc_intr, NULL, sc,
+		&sc->sc_intrhand)) {
 		device_printf(dev, "can't establish interrupt\n");
-		goto error;
+		goto err_setup_intr;
+	}
+
+	name = device_get_nameunit(sc->sc_dev);
+	for (irq = 0; irq < MAX_INTR; irq++) {
+		sc->sc_isrcs[irq].di_irq = irq;
+		sc->sc_isrcs[irq].di_level = 0; // ???
+		rc = intr_isrc_register(&sc->sc_isrcs[irq].di_isrc,
+			sc->sc_dev, 0, "%s,%d", name, irq);
+		if (rc != 0) {
+			device_printf(dev, "failed to register irq %d\n", irq);
+			goto err_register_intr;
+		}
+	}
+
+	if (intr_pic_register(sc->sc_dev, OF_xref_from_node(node)) == NULL) {
+		device_printf(dev, "failed to register pic\n");
+		goto err_pic_register;
 	}
 
 	return (0);
 
-error:
-	bus_release_resources(dev, apple_dc_res_spec, sc->res);
+err_pic_register:
+	// need to unregister isrcs here?
+err_register_intr:
+	//bus_teardown_intr(
+err_setup_intr:
+	bus_release_resources(dev, apple_dc_res_spec, sc->sc_res);
 	return (ENXIO);
 }
 
@@ -148,64 +194,60 @@ apple_dc_detach(device_t dev)
 	panic("uh oh");
 }
 
-static void
+static int
 apple_dc_intr(void *arg)
 {
-	uint32_t irq;
-	uint32_t stat, pending;
+	struct apple_dc_softc *sc;
+	struct apple_dc_irqsrc *disrc;
+	struct trapframe *tf;
+	uint32_t irq, stat, pending;
 
-	struct apple_dc_softc *sc = arg;
-	struct trapframe *tf = curthread->td_intr_frame;
-	struct apple_dc_irqsrc *dcisrc;
+	sc = arg;
+	tf = curthread->td_intr_frame;
 
 	stat = HREAD4(sc, IRQ_STAT);
 	pending = stat;
 	while (pending) {
 		irq = ffs(pending) - 1;
 
-		dcisrc = &sc->dcisrcs[irq];
+		disrc = &sc->sc_isrcs[irq];
 
-		if (intr_isrc_dispatch(&dcisrc->isrc, tf) != 0) {
+		if (intr_isrc_dispatch(&disrc->di_isrc, tf) != 0) {
+			device_printf(sc->sc_dev, "Stray irq %d disabled\n", irq);
+			return FILTER_STRAY;
 		}
 
 		pending &= ~(1 << irq);
 	}
 	HWRITE4(sc, IRQ_STAT, stat);
+
+	return FILTER_HANDLED;
 }
 
 static void
 apple_dc_enable_intr(device_t dev, struct intr_irqsrc *isrc)
 {
 	struct apple_dc_softc *sc = device_get_softc(dev);
-	struct apple_dc_irqsrc *dcisrc = (struct apple_dc_irqsrc *)isrc;
-	uint32_t irq = dcisrc->irq;
-	uint32_t mask;
+	struct apple_dc_irqsrc *di = (struct apple_dc_irqsrc *)isrc;
+	uint32_t irq = di->di_irq;
 
-	MPASS(irq < MAX_INTR);
-	mask = HREAD4(sc, IRQ_MASK);
-	mask |= 1 << irq;
-	HWRITE4(sc, IRQ_MASK, mask);
+	apple_dc_unmask_irq(sc, irq);
 }
 
 static void
 apple_dc_disable_intr(device_t dev, struct intr_irqsrc *isrc)
 {
 	struct apple_dc_softc *sc = device_get_softc(dev);
-	struct apple_dc_irqsrc *dcisrc = (struct apple_dc_irqsrc *)isrc;
-	uint32_t irq = dcisrc->irq;
-	uint32_t mask;
+	struct apple_dc_irqsrc *di = (struct apple_dc_irqsrc *)isrc;
+	uint32_t irq = di->di_irq;
 
-	MPASS(irq < MAX_INTR);
-	mask = HREAD4(sc, IRQ_MASK);
-	mask &= ~(1 << irq);
-	HWRITE4(sc, IRQ_MASK, mask);
+	apple_dc_mask_irq(sc, irq);
 }
 
 static int
-apple_dc_map_intr(device_t dev, struct intr_map_data *data,
-    struct intr_irqsrc **isrcp)
+do_apple_dc_map_intr(device_t dev, struct intr_map_data *data,
+    uint32_t *irqp, uint32_t *levelp)
 {
-	struct apple_dc_softc *sc = device_get_softc(dev);
 	if (data->type != INTR_MAP_DATA_FDT) {
 		return ENOTSUP;
 	}
@@ -219,28 +261,76 @@ apple_dc_map_intr(device_t dev, struct intr_map_data *data,
 	if (irq >= MAX_INTR) {
 		return EINVAL;
 	}
-	*isrcp = &sc->dcisrcs[irq].isrc;
+	if (irqp) {
+		*irqp = irq;
+	}
+	uint32_t level = daf->cells[1];
+	if (levelp) {
+		*levelp = level;
+	}
 	return 0;
 }
 
-#if 0
+static int
+apple_dc_map_intr(device_t dev, struct intr_map_data *data,
+    struct intr_irqsrc **isrcp)
+{
+	int rc;
+	uint32_t irq;
+	struct apple_dc_softc *sc = device_get_softc(dev);
+
+	if (isrcp == NULL) {
+		return EINVAL;
+	}
+
+	rc = do_apple_dc_map_intr(dev, data, &irq, NULL);
+	if (rc != 0) {
+		return rc;
+	}
+
+	*isrcp = &sc->sc_isrcs[irq].di_isrc;
+	return 0;
+}
+
 static int
 apple_dc_setup_intr(device_t dev, struct intr_irqsrc *isrc,
     struct resource *res, struct intr_map_data *data)
 {
-	uint32_t irq;
-	struct apple_dc_softc *sc = device_get_softc(dev);
-	struct apple_dc_irqsrc *dcisrc = (struct apple_dc_irqsrc *)isrc;
+	int rc;
+	uint32_t irq, level;
+	struct apple_dc_irqsrc *di;
 
+	di = (struct apple_dc_irqsrc *)isrc;
+
+	if (isrc->isrc_handlers != 0) {
+		return 0;
+	}
 	if (data == NULL) {
 		return ENOTSUP;
 	}
+	rc = do_apple_dc_map_intr(dev, data, &irq, &level);
+	if (rc != 0) {
+		return rc;
+	}
+	di->di_irq = irq;
+	di->di_level = level;
+	return 0;
 }
 
 static int
 apple_dc_teardown_intr(device_t dev, struct intr_irqsrc *isrc,
     struct resource *res, struct intr_map_data *data)
 {
+	int rc;
+	uint32_t irq, level;
+	struct apple_dc_softc *sc = device_get_softc(dev);
+
+	rc = do_apple_dc_map_intr(dev, data, &irq, &level);
+	if (rc != 0) {
+		return rc;
+	}
+	apple_dc_mask_irq(sc, irq);
+	return 0;
 }
 
 static device_method_t apple_dc_methods[] = {
@@ -266,6 +356,7 @@ static driver_t apple_dc_driver = {
 
 DRIVER_MODULE(apple_dc, simplebus, apple_dc_driver, 0, 0);
 
+#if 0
 #define APPLE_DOCKCHANNEL_HID_NRES 4
 //#define APPLE_DOCKCHANNEL_HID_IRQRES 4
 #define APPLE_DOCKCHANNEL_HID_CONFIG 0
