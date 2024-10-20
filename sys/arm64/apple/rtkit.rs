e6ee70fc739375535a6eaeb1682a248ad527f044
Ayrton Munoz <a.munoz3327@gmail.com>
2024-10-20 02:57:57 -0400

    wip

--- sys/arm64/apple/rtkit.rs
+++ sys/arm64/apple/rtkit.rs
@@ -20,29 +20,21 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
 use core::ffi::{c_int, c_void};
 use core::mem::MaybeUninit;
 use core::ptr::{addr_of_mut, null_mut};
-use kpi::device::Device;
-use kpi::taskq::Task;
-use kpi::prelude::*;
-use kpi::{bindings, enum_c_macros, RefMut, get_field, AsRustType, PointsTo};
 use kpi::allocator::NOWAIT;
+use kpi::device::Device;
+use kpi::prelude::*;
+use kpi::taskq;
+use kpi::taskq::Task;
+use kpi::{bindings, enum_c_macros, get_field};
 
-use apple_mbox::apple_mbox_driver;
+use apple_mbox::{apple_mbox_driver, AppleMboxMsg};
 
-#[repr(C)]
-struct RTKitCtx {
-    msg: bindings::apple_mbox_msg,
-    state: *mut bindings::rtkit_state,
-}
-
-struct RTKitTask {
-    task: Task,
-    ctx: bindings::rtkit_task,//RTKitCtx,
-}
-
+// I don't want a real enum here since other values may also be endpoints but CamelCase is not
+// standard style
+#[allow(nonstandard_style)]
 mod Endpoint {
     pub const MGMT: u32 = 0;
     pub const CRASHLOG: u32 = 1;
@@ -53,79 +45,64 @@ mod Endpoint {
     pub const TRACEKIT: u32 = 10;
 }
 
-extern "C" fn rx_task(mut ctx: RefMut<RTKitTask>, pending: c_int) {
-    /*
-    let state = ctx.state;
-    let msg = &raw mut ctx.msg;
-    let endpoint = ctx.msg.data1;
-    let res = match endpoint {
-        Endpoint::MGMT => {
-            = unsafe { bindings::rtkit_handle_mgmt(state, msg) };
-        },
-        Endpoint::CRASHLOG => {
-        },
-        Endpoint::SYSLOG => {
-        },
-        Endpoint::IOREPORT => {
-        },
-        Endpoint::OSLOG => {
-        },
-        Endpoint::TRACEKIT => {
-        },
-        _ => {
-        },
-    };
-    if res != 0 {
-    }
-    unsafe {
-        bindings::rtkit_rx_task(get_field!(ctx, ctx).as_ptr(), pending);
-    }
-    */
-}
-
-extern "C" fn rx_callback(cookie: *mut c_void, msg: bindings::apple_mbox_msg) -> c_int {
-    let mut rtk = Box::new_in(RTKitTask {
-        task: Task::uninit(),
-        ctx: bindings::rtkit_task { msg, state: cookie.cast() },
-    }, NOWAIT
-    );
-    /*
-    let mut rktask = RefMut::new_in_heap(RTKitTask {
-        task: Task::new(), ctx: /*RTKitCtx*/bindings::rtkit_task { msg, state: cookie.cast() }
-    }, NOWAIT);
-    get_field!(rktask, task).init_and_enqueue(rx_task, rktask, unsafe { bindings::taskqueue_thread }).unwrap();
-    */
-    0
+#[repr(u16)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum PwrState {
+    Sleep = 0x0001,
+    Quiesced = 0x0010,
+    On = 0x0020,
 }
 
 #[repr(C)]
 #[derive(Debug)]
 pub struct RTKit {
-    cstruct: bindings::rtkit_state,
+    client: Device,
+    mbox: Device,
+
+    iop_pwrstate: PwrState,
+    ap_pwrstate: PwrState,
+    verbose: bool,
+    noalloc: bool,
+    /*
+    epmap: u64,
+    callbacks: [Callback; 32],
+    */
+}
+
+// RTKitTask is a subclass of `struct task`
+type RTKitTask = SubClass<Task, RTKitTaskFields>;
+
+#[derive(Debug)]
+struct RTKitTaskFields {
+    rtkit: Ptr<RTKit>,
+    msg: AppleMboxMsg,
 }
 
 impl RTKit {
-    pub fn new(dev: Device, noalloc: bool) -> Box<Self> {
-        let mut init: bindings::rtkit_state = unsafe { MaybeUninit::zeroed().assume_init() };
-        init.dev = dev.as_ptr();
-        init.iop_pwrstate = bindings::RTKIT_MGMT_PWR_STATE_SLEEP as u16;
-        init.ap_pwrstate = bindings::RTKIT_MGMT_PWR_STATE_QUIESCED as u16;
-        init.verbose = false;
-        init.noalloc = noalloc;
-        Box::new(Self { cstruct: init })
+    pub fn new(client: Device, noalloc: bool) -> Result<Box<Self>> {
+        let mbox = apple_mbox_driver.get(client)?;
+        let iop_pwrstate = PwrState::Sleep;
+        let ap_pwrstate = PwrState::Quiesced;
+        Ok(Box::new_in(
+            Self {
+                client,
+                mbox,
+
+                iop_pwrstate,
+                ap_pwrstate,
+
+                verbose: false,
+                // TODO: only used in smc, may be unnecessary
+                noalloc,
+            },
+            WAITOK,
+        ))
     }
 
     pub fn boot(&mut self) -> Result<()> {
-        let dev = self.cstruct.dev.as_rust_type();
-        let mbox = apple_mbox::Driver::get(dev)?;
-        let state = addr_of_mut!(self.cstruct);
-        unsafe {
-            (*state).mbox.dev = mbox.as_ptr();
-        }
-        apple_mbox_driver
-            .set_rx(mbox, rx_callback, state.cast())
-            .unwrap();
-        let res = self.set_iop_pwrstate(bindings::RTKIT_MGMT_PWR_STATE_ON as u16);
+        let rtkit = unsafe { Ptr::new(self as *mut Self) };
+        apple_mbox_driver.set_rx(self.mbox, rx_callback, rtkit)?;
+        let res = self.set_iop_pwrstate(PwrState::On);
         if res != 0 {
             Err(ErrCode::from(res))
         } else {
@@ -133,18 +110,117 @@ impl RTKit {
         }
     }
 
-    pub fn set_iop_pwrstate(&mut self, pwrstate: u16) -> c_int {
-        unsafe { bindings::rtkit_set_iop_pwrstate(addr_of_mut!(self.cstruct), pwrstate) }
+    pub fn set_iop_pwrstate(&mut self, pwrstate: PwrState) -> c_int {
+        0
     }
-    pub fn set_ap_pwrstate(&mut self, pwrstate: u16) -> c_int {
-        unsafe { bindings::rtkit_set_ap_pwrstate(addr_of_mut!(self.cstruct), pwrstate) }
+
+    pub fn set_ap_pwrstate(&mut self, pwrstate: PwrState) -> c_int {
+        0
+    }
+}
+
+fn rx_callback(rtkit: Ptr<RTKit>, msg: AppleMboxMsg) -> Result<()> {
+    let ctx = RTKitTaskFields { rtkit, msg };
+    // Put the task and its context in a `Box` (i.e. on the heap). The `struct task` is
+    // zero-initialized while the other context fields are initialized with the arguments
+    let mut task = Box::new_in(RTKitTask::new(ctx), NOWAIT);
+    // Set the task's own address as the context and enqueue on taskqueue_thread with the
+    // callback rx_task. `enqueue_with_self` consumes `task` so it can't be used again in this
+    // function and internally `enqueue_with_self` calls `Box::into_raw` to avoid freeing `task`
+    // itself. When the callback is invoked it's passed the boxed task as an argument so it
+    // either explicitly passes ownership of the Box to something else or implicitly drops it at
+    // the end of the function (i.e. frees the allocated task and context)
+    task.enqueue_with_self(rx_task, taskq::thread())?;
+    Ok(())
+}
+
+mod mgmt {
+    pub const HELLO: u64 = 1;
+    pub const HELLO_ACK: u64 = 2;
+    pub const STARTEP: u64 = 5;
+    pub const IOP_PWR_STATE: u64 = 6;
+    pub const IOP_PWR_STATE_ACK: u64 = 7;
+    pub const EPMAP: u64 = 8;
+    pub const AP_PWR_STATE: u64 = 11;
+    pub fn msg_type(x: u64) -> u64 {
+        (x >> 52) & 0xff
+    }
+    pub fn hello_minver(x: u64) -> u64 {
+        x & 0xffff
+    }
+    pub fn hello_maxver(x: u64) -> u64 {
+        (x >> 16) & 0xffff
+    }
+}
+
+/* versions we support */
+const MINVER: u64 = 11;
+const MAXVER: u64 = 12;
+
+fn handle_mgmt(ctx: &RTKitTask) -> Result<()> {
+    let data0 = ctx.msg.data0;
+    let ty = mgmt::msg_type(data0);
+    match ty {
+        mgmt::HELLO => {
+            let minver = mgmt::hello_minver(data0);
+            let maxver = mgmt::hello_maxver(data0);
+            if minver > MAXVER {
+                return Err(EINVAL);
+            }
+            if maxver < MINVER {
+                return Err(EINVAL);
+            }
+            let mut ver = maxver;
+            if MAXVER < ver {
+                ver = MAXVER;
+            }
+            apple_mbox_driver.write(get_field!(ctx.rtkit, mbox).flatten(), todo!(""));
+        },
+        mgmt::HELLO_ACK => {
+        },
+        mgmt::STARTEP => {
+        },
+        mgmt::IOP_PWR_STATE => {
+        },
+        mgmt::IOP_PWR_STATE_ACK => {
+        },
+        mgmt::EPMAP => {
+        },
+        mgmt::AP_PWR_STATE => {
+        },
+        _ => {
+        },
+    }
+    Ok(())
+}
+
+extern "C" fn rx_task(ctx: Box<RTKitTask>, pending: c_int) {
+    let endpoint = ctx.msg.data1;
+    let res = match endpoint {
+        Endpoint::MGMT => handle_mgmt(&ctx),
+    //    Endpoint::CRASHLOG => {}
+    //    Endpoint::SYSLOG => {}
+    //    Endpoint::DEBUG => {}
+    //    Endpoint::IOREPORT => {}
+    //    Endpoint::OSLOG => {}
+    //    Endpoint::TRACEKIT => {}
+        _ => {
+            todo!("")
+        }
+    };
+    if let Err(err) = res {
+        // We have one extra level of indirection (i.e. `device_t *` or equivalently
+        // `struct _device **` so we need to flatten the `Ptr<Ptr<_device>>` into `Ptr<_device>`.
+        let client_dev: Ptr<Device> = get_field!(ctx.rtkit, client);
+        dprintln!(client_dev.flatten(),
+            "Error ({err:?}) while handling RTKit message from endpoint {endpoint:?}"
+        );
     }
 }
 
 #[no_mangle]
-extern "C" fn rtkit_init(dev: bindings::device_t, noalloc: bool) -> *mut bindings::rtkit_state {
-    let dev = dev.as_rust_type();
-    Box::into_raw(RTKit::new(dev, noalloc)).cast()
+extern "C" fn rtkit_init(dev: Device, noalloc: bool) -> *mut bindings::rtkit_state {
+    Box::into_raw(RTKit::new(dev, noalloc).unwrap()).cast()
 }
 
 #[no_mangle]
