ayrtonm/freebsd-src

commit 9b8d05b8ac78509e8c4b9f6f1e71c873b38a06b9

Add support for Amazon Elastic Network Adapter (ENA) NIC
ENA is a networking interface designed to make good use of modern CPU
features and system architectures.

The ENA device exposes a lightweight management interface with a
minimal set of memory mapped registers and extendable command set
through an Admin Queue.

The driver supports a range of ENA devices, is link-speed independent
(i.e., the same driver is used for 10GbE, 25GbE, 40GbE, etc.), and has
a negotiated and extendable feature set.

Some ENA devices support SR-IOV. This driver is used for both the
SR-IOV Physical Function (PF) and Virtual Function (VF) devices.

ENA devices enable high speed and low overhead network traffic
processing by providing multiple Tx/Rx queue pairs (the maximum number
is advertised by the device via the Admin Queue), a dedicated MSI-X
interrupt vector per Tx/Rx queue pair, and CPU cacheline optimized
data placement.

The ENA driver supports industry standard TCP/IP offload features such
as checksum offload and TCP transmit segmentation offload (TSO).
Receive-side scaling (RSS) is supported for multi-core scaling.

The ENA driver and its corresponding devices implement health
monitoring mechanisms such as watchdog, enabling the device and driver
to recover in a manner transparent to the application, as well as
debug logs.

Some of the ENA devices support a working mode called Low-latency
Queue (LLQ), which saves several more microseconds. This feature will
be implemented for driver in future releases.

Submitted by:	Michal Krawczyk <mk@semihalf.com>
		Jakub Palider <jpa@semihalf.com>
		Jan Medala <jan@semihalf.com>
Obtained from: Semihalf
Sponsored by: Amazon.com Inc.
Differential revision: https://reviews.freebsd.org/D10427

sys/dev/ena/ena.h
@@ -1,0 +1,434 @@
+/*-
+ * BSD LICENSE
+ *
+ * Copyright (c) 2015-2017 Amazon.com, Inc. or its affiliates.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ *
+ */
+
+#ifndef ENA_H
+#define ENA_H
+
+#include <sys/types.h>
+
+#include "ena-com/ena_com.h"
+#include "ena-com/ena_eth_com.h"
+
+#define DRV_MODULE_VER_MAJOR	0
+#define DRV_MODULE_VER_MINOR	7
+#define DRV_MODULE_VER_SUBMINOR 0
+
+#define DRV_MODULE_NAME		"ena"
+
+#ifndef DRV_MODULE_VERSION
+#define DRV_MODULE_VERSION				\
+	__XSTRING(DRV_MODULE_VER_MAJOR) "."		\
+	__XSTRING(DRV_MODULE_VER_MINOR) "."		\
+	__XSTRING(DRV_MODULE_VER_SUBMINOR)
+#endif
+#define DEVICE_NAME	"Elastic Network Adapter (ENA)"
+#define DEVICE_DESC	"ENA adapter"
+
+/* Calculate DMA mask - width for ena cannot exceed 48, so it is safe */
+#define ENA_DMA_BIT_MASK(x)		((1ULL << (x)) - 1ULL)
+
+/* 1 for AENQ + ADMIN */
+#define	ENA_MAX_MSIX_VEC(io_queues)	(1 + (io_queues))
+
+#define	ENA_REG_BAR			0
+#define	ENA_MEM_BAR			2
+
+#define	ENA_BUS_DMA_SEGS		32
+
+#define	ENA_DEFAULT_RING_SIZE		1024
+#define	ENA_DEFAULT_SMALL_PACKET_LEN	128
+#define	ENA_DEFAULT_MAX_RX_BUFF_ALLOC_SIZE	1536
+
+#define	ENA_RX_REFILL_THRESH_DEVIDER	8
+
+#define	ENA_MAX_PUSH_PKT_SIZE		128
+
+#define	ENA_NAME_MAX_LEN		20
+#define	ENA_IRQNAME_SIZE		40
+
+#define	ENA_PKT_MAX_BUFS		19
+#define	ENA_STALL_TIMEOUT		100
+
+#define	ENA_RX_RSS_TABLE_LOG_SIZE	7
+#define	ENA_RX_RSS_TABLE_SIZE		(1 << ENA_RX_RSS_TABLE_LOG_SIZE)
+
+#define	ENA_HASH_KEY_SIZE		40
+
+#define	ENA_DMA_BITS_MASK		40
+#define	ENA_MAX_FRAME_LEN		10000
+#define	ENA_MIN_FRAME_LEN		60
+#define	ENA_RX_HASH_KEY_NUM		10
+#define	ENA_RX_THASH_TABLE_SIZE		(1 << 8)
+
+#define ENA_TX_CLEANUP_TRESHOLD		128
+
+#define DB_THRESHOLD	64
+
+#define TX_COMMIT	32
+ /*
+ * TX budget for cleaning. It should be half of the RX budget to reduce amount
+ *  of TCP retransmissions.
+ */
+#define TX_BUDGET	128
+/* RX cleanup budget. -1 stands for infinity. */
+#define RX_BUDGET	256
+/*
+ * How many times we can repeat cleanup in the io irq handling routine if the
+ * RX or TX budget was depleted.
+ */
+#define CLEAN_BUDGET	8
+
+#define RX_IRQ_INTERVAL 20
+#define TX_IRQ_INTERVAL 50
+
+#define	ENA_MAX_MTU		9216
+#define	ENA_TSO_MAXSIZE		PAGE_SIZE
+#define	ENA_TSO_NSEGS		ENA_PKT_MAX_BUFS
+#define	ENA_RX_OFFSET		NET_SKB_PAD + NET_IP_ALIGN
+
+#define	ENA_MMIO_DISABLE_REG_READ	BIT(0)
+
+#define	ENA_TX_RING_IDX_NEXT(idx, ring_size) (((idx) + 1) & ((ring_size) - 1))
+
+#define	ENA_RX_RING_IDX_NEXT(idx, ring_size) (((idx) + 1) & ((ring_size) - 1))
+#define	ENA_RX_RING_IDX_ADD(idx, n, ring_size)	\
+	(((idx) + (n)) & ((ring_size) - 1))
+
+#define	ENA_IO_TXQ_IDX(q)		(2 * (q))
+#define	ENA_IO_RXQ_IDX(q)		(2 * (q) + 1)
+
+#define	ENA_MGMNT_IRQ_IDX		0
+#define	ENA_IO_IRQ_FIRST_IDX		1
+#define	ENA_IO_IRQ_IDX(q)		(ENA_IO_IRQ_FIRST_IDX + (q))
+
+/*
+ * ENA device should send keep alive msg every 1 sec.
+ * We wait for 6 sec just to be on the safe side.
+ */
+#define DEFAULT_KEEP_ALIVE_TO		(SBT_1S * 6)
+
+/* Time in jiffies before concluding the transmitter is hung. */
+#define DEFAULT_TX_CMP_TO		(SBT_1S * 5)
+
+/* Number of queues to check for missing queues per timer tick */
+#define DEFAULT_TX_MONITORED_QUEUES	(4)
+
+/* Max number of timeouted packets before device reset */
+#define DEFAULT_TX_CMP_THRESHOLD	(128)
+
+/*
+ * Supported PCI vendor and devices IDs
+ */
+#define	PCI_VENDOR_ID_AMAZON	0x1d0f
+
+#define	PCI_DEV_ID_ENA_PF	0x0ec2
+#define	PCI_DEV_ID_ENA_LLQ_PF	0x1ec2
+#define	PCI_DEV_ID_ENA_VF	0xec20
+#define	PCI_DEV_ID_ENA_LLQ_VF	0xec21
+
+struct msix_entry {
+	int entry;
+	int vector;
+};
+
+typedef struct _ena_vendor_info_t {
+	unsigned int vendor_id;
+	unsigned int device_id;
+	unsigned int index;
+} ena_vendor_info_t;
+
+struct ena_irq {
+	/* Interrupt resources */
+	struct resource *res;
+	driver_intr_t *handler;
+	void *data;
+	void *cookie;
+	unsigned int vector;
+	bool requested;
+	int cpu;
+	char name[ENA_IRQNAME_SIZE];
+};
+
+struct ena_que {
+	struct ena_adapter *adapter;
+	struct ena_ring *tx_ring;
+	struct ena_ring *rx_ring;
+	uint32_t id;
+	int cpu;
+};
+
+struct ena_tx_buffer {
+	struct mbuf *mbuf;
+	/* # of ena desc for this specific mbuf
+	 * (includes data desc and metadata desc) */
+	unsigned int tx_descs;
+	/* # of buffers used by this mbuf */
+	unsigned int num_of_bufs;
+	bus_dmamap_t map;
+
+	/* Used to detect missing tx packets */
+	struct bintime timestamp;
+	bool print_once;
+
+	struct ena_com_buf bufs[ENA_PKT_MAX_BUFS];
+} __aligned(CACHE_LINE_SIZE);
+
+struct ena_rx_buffer {
+	struct mbuf *mbuf;
+	bus_dmamap_t map;
+	struct ena_com_buf ena_buf;
+} __aligned(CACHE_LINE_SIZE);
+
+
+struct ena_stats_tx {
+	counter_u64_t cnt;
+	counter_u64_t bytes;
+	counter_u64_t queue_stop;
+	counter_u64_t prepare_ctx_err;
+	counter_u64_t queue_wakeup;
+	counter_u64_t dma_mapping_err;
+	/* Not counted */
+	counter_u64_t unsupported_desc_num;
+	/* Not counted */
+	counter_u64_t napi_comp;
+	/* Not counted */
+	counter_u64_t tx_poll;
+	counter_u64_t doorbells;
+	counter_u64_t missing_tx_comp;
+	counter_u64_t bad_req_id;
+};
+
+struct ena_stats_rx {
+	counter_u64_t cnt;
+	counter_u64_t bytes;
+	counter_u64_t refil_partial;
+	counter_u64_t bad_csum;
+	/* Not counted */
+	counter_u64_t page_alloc_fail;
+	counter_u64_t mbuf_alloc_fail;
+	counter_u64_t dma_mapping_err;
+	counter_u64_t bad_desc_num;
+	/* Not counted */
+	counter_u64_t small_copy_len_pkt;
+};
+
+
+struct ena_ring {
+	/* Holds the empty requests for TX out of order completions */
+	uint16_t *free_tx_ids;
+	struct ena_com_dev *ena_dev;
+	struct ena_adapter *adapter;
+	struct ena_com_io_cq *ena_com_io_cq;
+	struct ena_com_io_sq *ena_com_io_sq;
+
+	/* The maximum length the driver can push to the device (For LLQ) */
+	enum ena_admin_placement_policy_type tx_mem_queue_type;
+	uint16_t rx_small_copy_len;
+	uint16_t qid;
+	uint16_t mtu;
+	uint8_t tx_max_header_size;
+
+	struct ena_com_rx_buf_info ena_bufs[ENA_PKT_MAX_BUFS];
+	uint32_t  smoothed_interval;
+	enum ena_intr_moder_level moder_tbl_idx;
+
+	struct ena_que *que;
+	struct lro_ctrl lro;
+
+	uint16_t next_to_use;
+	uint16_t next_to_clean;
+
+	union {
+		struct ena_tx_buffer *tx_buffer_info; /* contex of tx packet */
+		struct ena_rx_buffer *rx_buffer_info; /* contex of rx packet */
+	};
+	int ring_size; /* number of tx/rx_buffer_info's entries */
+
+	struct buf_ring *br; /* only for TX */
+	struct mtx ring_mtx;
+	char mtx_name[16];
+	struct task enqueue_task;
+	struct taskqueue *enqueue_tq;
+	struct task cmpl_task;
+	struct taskqueue *cmpl_tq;
+
+	union {
+		struct ena_stats_tx tx_stats;
+		struct ena_stats_rx rx_stats;
+	};
+
+} __aligned(CACHE_LINE_SIZE);
+
+struct ena_stats_dev {
+	/* Not counted */
+	counter_u64_t tx_timeout;
+	/* Not counted */
+	counter_u64_t io_suspend;
+	/* Not counted */
+	counter_u64_t io_resume;
+	/* Not counted */
+	counter_u64_t wd_expired;
+	counter_u64_t interface_up;
+	counter_u64_t interface_down;
+	/* Not counted */
+	counter_u64_t admin_q_pause;
+};
+
+struct ena_hw_stats {
+	uint64_t rx_packets;
+	uint64_t tx_packets;
+
+	uint64_t rx_bytes;
+	uint64_t tx_bytes;
+
+	uint64_t rx_drops;
+};
+
+/* Board specific private data structure */
+struct ena_adapter {
+	struct ena_com_dev *ena_dev;
+
+	/* OS defined structs */
+	if_t ifp;
+	device_t pdev;
+	struct ifmedia	media;
+
+	/* OS resources */
+	struct resource * memory;
+	struct resource * registers;
+
+	struct mtx global_mtx;
+	struct sx ioctl_sx;
+
+	/* MSI-X */
+	uint32_t msix_enabled;
+	struct msix_entry *msix_entries;
+	int msix_vecs;
+
+	/* DMA tags used throughout the driver adapter for Tx and Rx */
+	bus_dma_tag_t tx_buf_tag;
+	bus_dma_tag_t rx_buf_tag;
+	int dma_width;
+	/*
+	 * RX packets that shorter that this len will be copied to the skb
+	 * header
+	 */
+	unsigned int small_copy_len;
+
+	uint16_t max_tx_sgl_size;
+	uint16_t max_rx_sgl_size;
+
+	uint32_t tx_offload_cap;
+
+	/* Tx fast path data */
+	int num_queues;
+
+	unsigned int tx_usecs, rx_usecs; /* Interrupt coalescing */
+
+	unsigned int tx_ring_size;
+	unsigned int rx_ring_size;
+
+	/* RSS*/
+	uint8_t	 rss_ind_tbl[ENA_RX_RSS_TABLE_SIZE];
+	bool rss_support;
+
+	uint32_t msg_enable;
+
+	uint8_t mac_addr[ETHER_ADDR_LEN];
+	/* mdio and phy*/
+
+	char name[ENA_NAME_MAX_LEN];
+	bool link_status;
+	bool trigger_reset;
+	bool up;
+	bool running;
+
+	uint32_t wol;
+
+	/* Queue will represent one TX and one RX ring */
+	struct ena_que que[ENA_MAX_NUM_IO_QUEUES]
+	    __aligned(CACHE_LINE_SIZE);
+
+	/* TX */
+	struct ena_ring tx_ring[ENA_MAX_NUM_IO_QUEUES]
+	    __aligned(CACHE_LINE_SIZE);
+
+	/* RX */
+	struct ena_ring rx_ring[ENA_MAX_NUM_IO_QUEUES]
+	    __aligned(CACHE_LINE_SIZE);
+
+	struct ena_irq irq_tbl[ENA_MAX_MSIX_VEC(ENA_MAX_NUM_IO_QUEUES)];
+
+	/* Timer service */
+	struct callout timer_service;
+	sbintime_t keep_alive_timestamp;
+	uint32_t next_monitored_tx_qid;
+	struct task reset_task;
+	struct taskqueue *reset_tq;
+	int wd_active;
+	sbintime_t keep_alive_timeout;
+	sbintime_t missing_tx_timeout;
+	uint32_t missing_tx_max_queues;
+	uint32_t missing_tx_threshold;
+
+	/* Statistics */
+	struct ena_stats_dev dev_stats;
+	struct ena_hw_stats hw_stats;
+};
+
+
+#define	ENA_DEV_LOCK			mtx_lock(&adapter->global_mtx)
+#define	ENA_DEV_UNLOCK			mtx_unlock(&adapter->global_mtx)
+
+#define	ENA_RING_MTX_LOCK(_ring)		mtx_lock(&(_ring)->ring_mtx)
+#define	ENA_RING_MTX_TRYLOCK(_ring)		mtx_trylock(&(_ring)->ring_mtx)
+#define	ENA_RING_MTX_UNLOCK(_ring)		mtx_unlock(&(_ring)->ring_mtx)
+
+struct ena_dev *ena_efa_enadev_get(device_t pdev);
+
+int ena_register_adapter(struct ena_adapter *adapter);
+void ena_unregister_adapter(struct ena_adapter *adapter);
+
+int ena_update_stats_counters(struct ena_adapter *adapter);
+
+static inline int ena_mbuf_count(struct mbuf *mbuf)
+{
+	int count = 1;
+
+	while ((mbuf = mbuf->m_next) != NULL)
+		++count;
+
+	return count;
+}
+
+#endif /* !(ENA_H) */

share/man/man4/ena.4
@@ -1,0 +1,255 @@
+.\" Copyright (c) 2015-2017 Amazon.com, Inc. or its affiliates.
+.\" All rights reserved.
+.\"
+.\" Redistribution and use in source and binary forms, with or without
+.\" modification, are permitted provided that the following conditions
+.\" are met:
+.\"
+.\" 1. Redistributions of source code must retain the above copyright
+.\"    notice, this list of conditions and the following disclaimer.
+.\"
+.\" 2. Redistributions in binary form must reproduce the above copyright
+.\"    notice, this list of conditions and the following disclaimer in
+.\"    the documentation and/or other materials provided with the
+.\"    distribution.
+.\"
+.\" THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+.\" "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+.\" LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+.\" A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+.\" OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+.\" SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+.\" LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+.\" DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+.\" THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+.\" (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+.\" OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+.\"
+.\" $FreeBSD$
+.\"
+.Dd May 04, 2017
+.Dt ENA 4
+.Os
+.Sh NAME
+.Nm ena
+.Nd "FreeBSD kernel driver for Elastic Network Adapter (ENA) family"
+.Sh SYNOPSIS
+To compile this driver into the kernel,
+place the following line in your
+kernel configuration file:
+.Bd -ragged -offset indent
+.Cd "device ena"
+.Ed
+.Pp
+Alternatively, to load the driver as a
+module at boot time, place the following line in
+.Xr loader.conf 5 :
+.Bd -literal -offset indent
+if_ena_load="YES"
+.Ed
+.Sh DESCRIPTION
+The ENA is a networking interface designed to make good use of modern CPU
+features and system architectures.
+.Pp
+The ENA device exposes a lightweight management interface with a
+minimal set of memory mapped registers and extendable command set
+through an Admin Queue.
+.Pp
+The driver supports a range of ENA devices, is link-speed independent
+(i.e., the same driver is used for 10GbE, 25GbE, 40GbE, etc.), and has
+a negotiated and extendable feature set.
+.Pp
+Some ENA devices support SR-IOV. This driver is used for both the
+SR-IOV Physical Function (PF) and Virtual Function (VF) devices.
+.Pp
+The ENA devices enable high speed and low overhead network traffic
+processing by providing multiple Tx/Rx queue pairs (the maximum number
+is advertised by the device via the Admin Queue), a dedicated MSI-X
+interrupt vector per Tx/Rx queue pair, and CPU cacheline optimized
+data placement.
+.Pp
+The
+.Nm
+driver supports industry standard TCP/IP offload features such
+as checksum offload and TCP transmit segmentation offload (TSO).
+Receive-side scaling (RSS) is supported for multi-core scaling.
+.Pp
+The
+.Nm
+driver and its corresponding devices implement health
+monitoring mechanisms such as watchdog, enabling the device and driver
+to recover in a manner transparent to the application, as well as
+debug logs.
+.Pp
+Some of the ENA devices support a working mode called Low-latency
+Queue (LLQ), which saves several more microseconds. This feature will
+be implemented for driver in future releases.
+.Sh HARDWARE
+Supported PCI vendor ID/device IDs:
+.Pp
+.Bl -bullet -compact
+.It
+1d0f:0ec2 - ENA PF
+.It
+1d0f:1ec2 - ENA PF with LLQ support
+.It
+1d0f:ec20 - ENA VF
+.It
+1d0f:ec21 - ENA VF with LLQ support
+.El
+.Sh DIAGNOSTICS
+.Ss Device initialization phase:
+.Bl -diag
+.It ena%d: failed to init mmio read less
+.Pp
+Error occured during initialization of the mmio register read request.
+.It ena%d: Can not reset device
+.Pp
+Device could not be reset; device may not be responding or is already
+during reset.
+.It ena%d: device version is too low
+.Pp
+Version of the controller is too low and it is not supported by the driver.
+.It ena%d: Invalid dma width value %d
+.Pp
+The controller is able to request dma transcation width. Device stopped
+responding or it demanded invalid value.
+.It ena%d: Can not initialize ena admin queue with device
+.Pp
+Initialization of the Admin Queue failed; device may not be responding or there
+was a problem with initialization of the resources.
+.It ena%d: Cannot get attribute for ena device rc: %d
+.Pp
+Failed to get attributes of the device from the controller.
+.It ena%d: Cannot configure aenq groups rc: %d
+.Pp
+Errors occured when trying to configure AENQ groups.
+.El
+.Ss Driver initialisation/shutdown phase:
+.Bl -diag
+.It ena%d: PCI resource allocation failed!
+.It ena%d: allocating ena_dev failed
+.It ena%d: failed to pmap registers bar
+.It ena%d: Error while setting up bufring
+.It ena%d: Error with initialization of IO rings
+.It ena%d: can not allocate ifnet structure
+.It ena%d: Error with network interface setup
+.It ena%d: Failed to enable and set the admin interrupts
+.It ena%d: Failed to allocate %d, vectors %d
+.It ena%d: Failed to enable MSIX, vectors %d rc %d
+.It ena%d: Error with MSI-X enablement
+.It ena%d: could not allocate irq vector: %d
+.It ena%d: Unable to allocate bus resource: registers
+.Pp
+Resource allocation failed when initializing the device; driver will not
+be attached.
+.It ena%d: ENA device init failed (err: %d)
+.Pp
+Device initialization failed; driver will not be attached.
+.It ena%d: could not activate irq vector: %d
+.Pp
+Error occured when trying to activate interrupt vectors for Admin Queue.
+.It ena%d: failed to register interrupt handler for irq %ju: %d
+.Pp
+Error occured when trying to register Admin Queue interrupt handler.
+.It ena%d: Cannot setup mgmnt queue intr
+.Pp
+Error occured during configuration of the Admin Queue interrupts.
+.It ena%d: Enable MSI-X failed
+.Pp
+Configuration of the MSI-X for Admin Queue failed; there could be lack
+of resources or interrupts could not have been configured; driver will
+not be attached.
+.It ena%d: VLAN is in use, detach first
+.Pp
+VLANs are being used when trying to detach the driver; VLANs should be detached
+first and then detach routine should be called again.
+.It ena%d: Unmapped RX DMA tag associations
+.It ena%d: Unmapped TX DMA tag associations
+.Pp
+Error occured when trying to destroy RX/TX DMA tag.
+.It ena%d: Cannot init RSS
+.It ena%d: Cannot fill indirect table
+.It ena%d: Cannot fill indirect table
+.It ena%d: Cannot fill hash function
+.It ena%d: Cannot fill hash control
+.It ena%d: WARNING: RSS was not properly initialized, it will affect bandwidth
+.Pp
+Error occured during initialization of one of RSS resources; device is still
+going to work but it will affect performance because all RX packets will be
+passed to queue 0 and there will be no hash information.
+.It ena%d: failed to tear down irq: %d
+.It ena%d: dev has no parent while releasing res for irq: %d
+Release of the interrupts failed.
+.El
+.Ss Additional diagnostic:
+.Bl -diag
+.It ena%d: Cannot get attribute for ena device
+.Pp
+This message appears when trying to change MTU and driver is unable to get
+attributes from the device.
+.It ena%d: Invalid MTU setting. new_mtu: %d
+.Pp
+Requested MTU value is not supported and will not be set.
+.It ena%d: keep alive watchdog timeout
+.Pp
+Device stopped responding and will be reset.
+.It ena%d: Found a Tx that wasn't completed on time, qid %d, index %d.
+.Pp
+Packet was pushed to the NIC but not sent within given time limit; it may
+be caused by hang of the IO queue.
+.It ena%d: The number of lost tx completion is aboce the threshold (%d > %d). Reset the device
+.Pp
+If too many Tx wasn't completed on time the device is going to be reset; it may
+be caused by hanged queue or device.
+.It ena%d: trigger reset is on
+.Pp
+Device will be reset; reset is triggered either by watchdog or if too many TX
+packets were not completed on time.
+.It ena%d: invalid value recvd
+.Pp
+Link status received from the device in the AENQ handler is invalid.
+.It ena%d: Allocation for Tx Queue %u failed
+.It ena%d: Allocation for Rx Queue %u failed
+.It ena%d: Unable to create Rx DMA map for buffer %d
+.It ena%d: Failed to create io TX queue #%d rc: %d
+.It ena%d: Failed to get TX queue handlers. TX queue num %d rc: %d
+.It ena%d: Failed to create io RX queue[%d] rc: %d
+.It ena%d: Failed to get RX queue handlers. RX queue num %d rc: %d
+.It ena%d: failed to request irq
+.It ena%d: could not allocate irq vector: %d
+.It ena%d: failed to register interrupt handler for irq %ju: %d
+.Pp
+IO resources initialization failed. Interface will not be brought up.
+.It ena%d: LRO[%d] Initialization failed!
+.Pp
+Initialization of the LRO for the RX ring failed.
+.It ena%d: failed to alloc buffer for rx queue
+.It ena%d: failed to add buffer for rx queue %d
+.It ena%d: refilled rx queue %d with %d pages only
+.Pp
+Allocation of resources used on RX path failed; if happened during
+initialization of the IO queue, the interface will not be brought up.
+.It ena%d: ioctl promisc/allmulti
+.Pp
+IOCTL request for the device to work in promiscuous/allmulti mode; see
+.Xr ifconfig 8
+for more details.
+.It ena%d: too many fragments. Last fragment: %d!
+.Pp
+Packet with unsupported number of segments was queued for sending to the
+device; packet will be dropped.
+.Sh SUPPORT
+If an issue is identified with the released source code with a supported adapter
+email the specific information related to the issue to
+.Aq Mt mk@semihalf.com
+and
+.Aq Mt mw@semihalf.com .
+.Sh SEE ALSO
+.Xr vlan 4 ,
+.Xr ifconfig 8
+.Sh AUTHORS
+The
+.Nm
+driver was written by
+.An Semihalf.

sys/conf/files
@@ -1584,6 +1584,12 @@ dev/e1000/e1000_mbx.c		optional em \
 dev/e1000/e1000_osdep.c		optional em \
 	compile-with "${NORMAL_C} -I$S/dev/e1000"
 dev/et/if_et.c			optional et
+dev/ena/ena.c			optional ena \
+	compile-with "${NORMAL_C} -I$S/contrib"
+dev/ena/ena_sysctl.c 		optional ena \
+	compile-with "${NORMAL_C} -I$S/contrib"
+contrib/ena-com/ena_com.c	optional ena
+contrib/ena-com/ena_eth_com.c	optional ena
 dev/ep/if_ep.c			optional ep
 dev/ep/if_ep_isa.c		optional ep isa
 dev/ep/if_ep_pccard.c		optional ep pccard

commit 74bcb1151bb94279a4269fc842aa2be00800545e

ena: Fix false 'missing TX completions' warnings due to timestamp race
Sporadic 'Found a Tx that wasn't completed on time' warnings appear
under sustained TX load, always reporting '1 msecs since last cleanup'
despite the 5-second timeout threshold.

The per-packet TX timestamp uses struct bintime (128 bits: two 64-bit
fields sec and frac) which is read and written non-atomically. A race
exists between the missing TX completion check
(check_missing_comp_in_tx_queue reading the timestamp) and the TX
submit path or cleanup path writing it on another CPU. Since the two
fields are not updated atomically, the check can observe a partially
written timestamp - one field from the old value and one from the new.
This can produce a timestamp with {sec=0, frac=valid}, causing the
check to compute a time offset equal to system uptime and falsely
exceeding the 5-second timeout.

Confirmed by instrumentation showing all occurrences had sec=0 with
valid frac/mbuf, cleanup_running=0, and ticks==last_cleanup_ticks.

Replace struct bintime with sbintime_t (a single 64-bit value) for
tx_buf->timestamp. An aligned 64-bit store/load cannot be torn on
64-bit architectures. Additionally, snapshot the timestamp into a
local variable in the check path to prevent a read-then-read race
where the timestamp could be zeroed between the zero-check and the
offset calculation.

Testing:
On m6i.large (FreeBSD 15.0-RELEASE-p6 amd64, 2 IO queues), two
instances with MTU 1500. Ran iperf -P 20 -u -b 320kpps (CPU
saturated at ~7 Gbps aggregate).

Without the fix: 8 warnings in 6 hours (first at ~72 min).
With the fix: 0 warnings after 20+ hours under identical conditions.

Fixes: 9b8d05b8ac78 ("Add support for Amazon Elastic Network Adapter (ENA) NIC")
Submitted by: Gilad Ben Yakov <giladben@amazon.com>
MFC after: 2 weeks
Sponsored by: Amazon, Inc.
Reviewed by: cperciva
Differential Revision: https://reviews.freebsd.org/D58241

sys/dev/ena/ena.c
@@ -3213,7 +3213,7 @@ check_missing_comp_in_tx_queue(struct ena_adapter *adapter,
 {
 	uint32_t missed_tx = 0, new_missed_tx = 0;
 	device_t pdev = adapter->pdev;
-	struct bintime curtime, time;
+	sbintime_t curtime;
 	struct ena_tx_buffer *tx_buf;
 	int time_since_last_cleanup;
 	int missing_tx_comp_to;
@@ -3222,17 +3222,18 @@ check_missing_comp_in_tx_queue(struct ena_adapter *adapter,
 	enum ena_regs_reset_reason_types reset_reason = ENA_REGS_RESET_MISS_TX_CMPL;
 	bool cleanup_scheduled, cleanup_running;
 
-	getbinuptime(&curtime);
+	curtime = getsbinuptime();
 
 	for (i = 0; i < tx_ring->ring_size; i++) {
+		sbintime_t ts;
+
 		tx_buf = &tx_ring->tx_buffer_info[i];
 
-		if (bintime_isset(&tx_buf->timestamp) == 0)
+		ts = atomic_load_64(&tx_buf->timestamp);
+		if (ts == 0)
 			continue;
 
-		time = curtime;
-		bintime_sub(&time, &tx_buf->timestamp);
-		time_offset = bttosbt(time);
+		time_offset = curtime - ts;
 
 		if (unlikely(!atomic_load_8(&tx_ring->first_interrupt) &&
 		    time_offset > 2 * adapter->missing_tx_timeout)) {

sys/dev/ena/ena.h
@@ -263,7 +263,7 @@ struct ena_tx_buffer {
 	bus_dmamap_t dmamap;
 
 	/* Used to detect missing tx packets */
-	struct bintime timestamp;
+	sbintime_t timestamp;
 	bool print_once;
 
 #ifdef DEV_NETMAP

sys/dev/ena/ena_datapath.c
@@ -274,7 +274,7 @@ ena_tx_cleanup(struct ena_ring *tx_ring)
 		mbuf = tx_info->mbuf;
 
 		tx_info->mbuf = NULL;
-		bintime_clear(&tx_info->timestamp);
+		atomic_store_64(&tx_info->timestamp, 0);
 
 		bus_dmamap_sync(adapter->tx_buf_tag, tx_info->dmamap,
 		    BUS_DMASYNC_POSTWRITE);
@@ -1055,7 +1055,7 @@ ena_xmit_mbuf(struct ena_ring *tx_ring, struct mbuf **mbuf)
 	counter_exit();
 
 	tx_info->tx_descs = nb_hw_desc;
-	getbinuptime(&tx_info->timestamp);
+	atomic_store_64(&tx_info->timestamp, getsbinuptime());
 	tx_info->print_once = true;
 
 	tx_ring->next_to_use = ENA_TX_RING_IDX_NEXT(next_to_use,